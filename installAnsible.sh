f78b8dada6754a613e0e43c0be4ed39055371802
felixdong <felixdong423@gmail.com>
2024-01-27 20:28:35 +0800

    script update

--- installAnsible.sh
+++ installAnsible.sh
@@ -6,3 +6,4 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 git remote set-url origin git@github.com:curelesss/ansible.git
+ansible-playbook init.yml --ask-vault-pass
