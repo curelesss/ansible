d4873b58b7ad9001f7a2e36278979894b8862a40
felixdong <felixdong423@gmail.com>
2024-01-27 20:20:56 +0800

    install ansible script update

--- installAnsible.sh
+++ installAnsible.sh
@@ -1,6 +1,7 @@
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
 sudo cp sources.list.mirror /etc/apt/sources.list
 sudo apt update
+sudo apt upgrade -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
