9b11ed39fb90da99f24f85edfee7579fea3cf3e0
felixdong <felixdong423@gmail.com>
2024-02-01 18:46:16 +0800

    add install ansible aur module to the script

--- installArchSoftware.sh
+++ installArchSoftware.sh
@@ -1,1 +1,3 @@
+mkdir -p ~/.ansible/plugins/modules
+curl -o ~/.ansible/plugins/modules/aur.py https://raw.githubusercontent.com/kewlfft/ansible-aur/master/plugins/modules/aur.py
 ansible-playbook install.arch.yml --ask-become-pass
